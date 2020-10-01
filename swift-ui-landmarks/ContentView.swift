9bcb7c97bd905b16eda5e0c2f25fc1d5f3786ff7
dustydubs <dustin.waldron@homeadvisor.com>
2020-09-30 22:51:22 -0400

    Initial Commit

---
+++ swift-ui-landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-ui-landmarks
+//
+//  Created by Waldron, Dustin on 9/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-ui-landmarks/swift_ui_landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_landmarksApp.swift
+//  swift-ui-landmarks
+//
+//  Created by Waldron, Dustin on 9/30/20.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
